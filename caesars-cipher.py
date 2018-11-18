6ed6ffef86461c66bd9b09fc153631e091b2956e
Jesús Muñoz a.k.a. "omiK <munozcastro.jj@gmail.com>
2018-11-18 21:03:09 +0100

    Create caesars-cipher.py

---
+++ caesars-cipher.py
@@ -0,0 +1,24 @@
+def to_encrypt(text, delta):
+
+
+    output = ''
+
+    for char in text:
+        
+        # Storing current char Number
+        charN = ord(char)
+
+        # Lets add (delta) to char Number each loop 
+        if charN >= 97:
+            charN += delta
+
+        # If over 122 or under 97 - or + 26 to keep at same (delta) distance
+        if charN > 122:
+            charN -= 26
+
+        if charN < 97 and charN != 32:
+            charN += 26
+
+        output += chr(charN)
+
+    return output
